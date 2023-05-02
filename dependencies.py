8b2ca076716febcbd3f6dcee52acd596a3959c93
yurepin90@gmail.com <Bulldog201190>
2023-05-02 22:28:56 +0300

    added get photo handler

--- dependencies.py
+++ dependencies.py
@@ -1,34 +1,81 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
-# Set up the web driver (make sure to have the correct driver executable installed)
-driver = webdriver.Chrome('/path/to/chromedriver')
 
-# Navigate to the Instagram profile page
-username = 'username'
-driver.get('https://www.instagram.com/' + username + '/')
+#TODO перед вызовом каждой функции авторизоваться
+
+
+def get_photo_urls(username, max_cnt):
+    # Set up the web driver
+    driver = webdriver.Chrome('./chromedriver')
+
+    # Navigate to the Instagram profile page
+    driver.get('https://www.instagram.com/' + username + '/')
+    time.sleep(2)
+
+    # Scroll down to load more photos
+    #TODO подумать, как не скроллить до конца, а считывать фотографии на ходу
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    # Get the URLs of the photos
+    photo_links = []
+    for _ in range(max_cnt):
+        photo_elems = driver.find_elements(By.CSS_SELECTOR, 'a[href]')
+        for elem in photo_elems:
+            link = elem.get_attribute('href')
+            if '/p/' in link:
+                photo_links.append(link)
+
+    # Close the browser
+    driver.quit()
+
+    # Return resulting urls list
+    return photo_links
+
+# TODO проверить, что для закрытого акка возвращает пустой список
+
+
+# Set up the web driver (make sure to have the correct driver executable installed)
+driver = webdriver.Chrome('./chromedriver')
+
+# Navigate to the Instagram login page
+driver.get('https://www.instagram.com/accounts/login/')
 time.sleep(2)
 
-# Scroll down to load more photos
-last_height = driver.execute_script("return document.body.scrollHeight")
-while True:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(2)
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
+# Log in with your username and password
+username_field = driver.find_element_by_name('username')
+username_field.send_keys('your_username')
+password_field = driver.find_element_by_name('password')
+password_field.send_keys('your_password')
+password_field.send_keys(Keys.RETURN)
+time.sleep(2)
 
-# Get the URLs of the photos
-photo_links = []
-photo_elems = driver.find_elements_by_xpath('//a[@href][@tabindex="-1"]')
-for elem in photo_elems:
-    link = elem.get_attribute('href')
-    if '/p/' in link:
-        photo_links.append(link)
+# Navigate to the upload page
+driver.get('https://www.instagram.com/create')
+time.sleep(2)
 
-# Print the URLs of the photos
-print(photo_links)
+# Upload the photo
+upload_input = driver.find_element(By.XPATH, '//input[@type="file"]')
+upload_input.send_keys('/path/to/photo.jpg')
+time.sleep(2)
 
-# Close the browser
+# Add a caption (optional)
+caption_input = driver.find_element(By.CSS_SELECTOR, 'textarea[aria-label="Write a caption…"]')
+caption_input.send_keys('Caption for the photo')
+
+# Post the photo
+post_button = driver.find_element(By.XPATH, '//button[contains(text(), "Post")]')
+post_button.click()
+
+# Wait for the upload to complete and close the browser
+time.sleep(5)
 driver.quit()
--- routers/instagram.py
+++ routers/instagram.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, File, UploadFile
+from ..dependencies import get_photo_urls
 
 router = APIRouter()
 
@@ -7,14 +8,12 @@ router = APIRouter()
 async def root():
     return {"message": "Hello World"}
 
-# @router.get("/getPhotos")
-# async def get_photos(username: str = Query(..., description="Instagram username"),
-#                       max_count: int = Query(10, gt=0, le=100, description="Maximum number of photos to return")):
-#     # TODO: Implement logic to retrieve photos for the given username
-#     # For now, we'll just return an empty list
-#     urls = []
-#     response = {"urls": urls}
-#     return response
+@router.get("/getPhotos")
+async def get_photos(username: str = Query(..., description="Instagram username"),
+                      max_count: int = Query(10, gt=0, le=100, description="Maximum number of photos to return")):
+    urls = get_photo_urls(username, max_count)
+    response = {"urls": urls}
+    return response
 
 
 # @router.post("/postPhotos")
